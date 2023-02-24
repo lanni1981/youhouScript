0a84928b5dbd115b613e76eb7db6cc94bfd196f6
大雷 楼 <lou198110@hotmail.com>
2023-02-24 13:46:46 +0800

    纯文本复制0.1

---
+++ 纯文本复制.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         纯文本复制
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  仅复制纯文本，不带格式
+// @author       Lennie
+// @match        *://*
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // 阻止默认复制事件
+    document.addEventListener('copy', function(e) {
+        e.preventDefault();
+        // 获取当前选中的文本
+        var selectedText = window.getSelection().toString();
+        // 清除格式
+        var div = document.createElement('div');
+        div.innerHTML = selectedText;
+        selectedText = div.textContent || div.innerText || '';
+        // 将清除格式后的文本添加到剪贴板
+        e.clipboardData.setData('text/plain', selectedText);
+    });
+})();
