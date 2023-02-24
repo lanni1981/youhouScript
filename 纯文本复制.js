679ff2819a053cb2ddd9989eaa64e09c5625a7e5
大雷 楼 <lou198110@hotmail.com>
2023-02-24 15:37:39 +0800

    v0.14

--- 纯文本复制.js
+++ 纯文本复制.js
@@ -1,11 +1,7 @@
 // ==UserScript==
 // @name         纯文本复制
 // @namespace    http://tampermonkey.net/
-<<<<<<< HEAD
-// @version      0.13
-=======
-// @version      0.11
->>>>>>> parent of ec41910 (version      0.2)
+// @version      0.14
 // @description  仅复制纯文本，不带格式
 // @author       Lennie
 // @match        *://*
