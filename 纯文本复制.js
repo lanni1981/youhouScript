168c642fb764cbf148a3b47b1d477f03bc603b9b
大雷 楼 <lou198110@hotmail.com>
2023-02-24 15:23:49 +0800

    @version      0.13

--- 纯文本复制.js
+++ 纯文本复制.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         纯文本复制
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.13
 // @description  仅复制纯文本，不带格式
 // @author       Lennie
 // @match        *://*
